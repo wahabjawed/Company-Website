23dcd091a487d8e7ceae7b45b132599f0516b403
Wahab Jawed <wahabjawed@gmail.com>
2014-08-08 18:21:19 +0500

    Baba Work

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+$('.service-column').click(function(){
+	$('#servicesDescrip').removeClass('collapse-vertical').addClass('expand-vertical');
+	$('#services').addClass('collapse-vertical').removeClass('expand-vertical');
+	});
+$('#servicesDescrip ').click(function(){
+	$('#servicesDescrip').addClass('collapse-vertical').removeClass('expand-vertical');
+	$('#services').removeClass('collapse-vertical').addClass('expand-vertical');
+	});
